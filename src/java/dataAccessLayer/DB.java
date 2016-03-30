217e2393426a60852f4014ddd42a4df154d810de
Daniel Winkel <Danielwinkel5@hotmail.com>
2016-03-30 22:15:33 +0200

    Rettelser: DB

---
+++ src/java/dataAccessLayer/DB.java
@@ -0,0 +1,8 @@
+package dataAccessLayer;
+
+public class DB {
+    public static String driver = "com.mysql.jdbc.Driver";
+    public static String URL = "jdbc:mysql://localhost/polygondb";
+    public static String ID = "root";
+    public static String PW = "15243qtwre";
+}

c5d20e9894c1cf83e4a4366b26ccb9c8f9aae6b9
danie <danie@10.20.147.39>
2016-04-04 12:38:43 +0200

    gettAllFloorByBuildingID added

--- src/java/dataAccessLayer/mapper/FloorMapper.java
+++ src/java/dataAccessLayer/mapper/FloorMapper.java
@@ -2,22 +2,27 @@ package dataAccessLayer.mapper;
 
 import dataAccessLayer.DBConnector;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import serviceLayer.entity.Floor;
 
 public class FloorMapper {
 //Made by Nicolai
+
     public boolean addFloors(List<Floor> floors) {
         try {
             String query = "INSERT INTO floor (floor, size, buildingId) VALUES (?, ?, ?)";
             PreparedStatement ps = DBConnector.getConnection().prepareStatement(query);
-        
-            for(Floor floor : floors){
+
+            for (Floor floor : floors) {
                 ps.setInt(1, floor.getFloor());
                 ps.setDouble(2, floor.getSize());
                 ps.setInt(3, floor.getBuildingId());
-                
+
                 ps.executeUpdate();
             }
         } catch (SQLException ex) {
@@ -26,6 +31,7 @@ public class FloorMapper {
         }
         return true;
     }
+
     //Made by Nicolai
     public void deleteFloorsByBuildingId(int buildingId) {
         try {
@@ -33,11 +39,38 @@ public class FloorMapper {
             PreparedStatement ps = DBConnector.getConnection().prepareStatement(query);
 
             ps.setInt(1, buildingId);
-            
+
             ps.executeUpdate();
 
             ps.close();
         } catch (SQLException ee) {
         }
+    }
+
+    public List<Floor> getAllFloorsByBuildingID(int buildingId) {
+            List<Floor> floors = new ArrayList();
+        Floor floor;
+        ResultSet rs = null;
+        try {
+            
+            String query = "SELECT * FROM floor WHERE (buildingId) = ?";
+            PreparedStatement ps = DBConnector.getConnection().prepareStatement(query);
+            
+            while(rs.next()) {
+                
+                int floorId = rs.getInt("floorId");
+                int floorNr = rs.getInt("floor");
+                double floorSize = rs.getDouble("size");
+                int buildingID = rs.getInt("buildingId");
+                
+                floor = new Floor(floorId, floorNr, floorSize, buildingId);
+                floors.add(floor);
+                
+                ps.executeQuery();
+            }
+        } catch (SQLException ex) {
+            ex.printStackTrace();
+        }
+        return floors;
     }
 }

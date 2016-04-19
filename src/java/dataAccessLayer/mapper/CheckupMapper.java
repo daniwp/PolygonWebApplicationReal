eef2e96f9ced8bbc3c48aa594d42d90ddcae4688
PeterHenriksen <PeterHenriksen@10.20.172.234>
2016-04-19 12:15:32 +0200

    der er addet updateCheckupStatusById i checkupMapperen

--- src/java/dataAccessLayer/mapper/CheckupMapper.java
+++ src/java/dataAccessLayer/mapper/CheckupMapper.java
@@ -1,6 +1,10 @@
 
 package dataAccessLayer.mapper;
 
+import dataAccessLayer.DBConnector;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.List;
 import serviceLayer.entity.Checkup;
 
@@ -11,7 +15,21 @@ import serviceLayer.entity.Checkup;
 public class CheckupMapper {
     
     // Peter
-    public void updateCheckupStatusById(int checupId, String status) {
+    public void updateCheckupStatusById(int checkupId, String status) {
+        
+        
+        try {
+            String query = "UPDATE checkup SET status = ? Where checkupId = ?";
+            PreparedStatement ps = DBConnector.getConnection().prepareStatement(query);
+            
+            ps.setString(1, status);
+            ps.setInt(2, checkupId);
+            
+            ps.executeUpdate();
+            
+        }catch (SQLException ee) {
+            ee.printStackTrace();
+        }
         
     }
     
